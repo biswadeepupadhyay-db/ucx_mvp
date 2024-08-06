0b4b8feda2ee0d6f0e74b7efb370b7917f4a31ff
biswadeepupadhyay-db <biswadeepupadhyay-db>
2024-08-06 11:58:45 +0000

    Added DLT code for Live and Stream Table

---
+++ dlt_viking_mvp/beepz_dlt_sql_live_stream_test.sql
@@ -0,0 +1,20 @@
+-- Databricks notebook source
+CREATE OR REFRESH STREAMING LIVE TABLE employee_bronze
+AS SELECT
+  *
+FROM
+  STREAM read_files(
+    'dbfs:/FileStore/beepz/ingestion/source_data_emp.csv',
+    format => 'csv',
+    header => true,
+    sep => ',',
+    mode => 'DROPMALFORMED'
+  )
+
+-- COMMAND ----------
+
+CREATE OR REFRESH STREAMING LIVE TABLE employee_silver
+AS SELECT
+  *
+FROM STREAM(LIVE.employee_bronze)
+where salary > 10000
